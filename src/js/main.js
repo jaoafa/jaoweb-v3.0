1d95dcb6808ffc1e06c8f0432be3e0e450926a3c
Hiratake <hiratake@hiratake.xyz>
2019-06-29 16:12:37 +0900

    Fix: class名を変更したため動作しなくなった箇所を修正🐛

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,18 @@
 /* -^------------------^-
  * Toggle Navigation
  * ---------------------- */
-var hamburger = document.getElementById( 'header-navigation-toggle' );
-var hamburgerBars = hamburger.querySelector( 'span' );
-var headerNavigation = document.getElementById( 'header-navigation' );
-var headerNavigationList = document.getElementsByClassName( 'header-navigation-list' );
+var hamburger            = document.getElementById( 'header-navigation-toggle' );
+var hamburgerBars        = hamburger.querySelector( 'span' );
+var headerNavigation     = document.getElementById( 'header-navigation' );
+var headerNavigationList = document.getElementsByClassName( 'p-header-nav__list-item--first' );
 hamburger.addEventListener( 'click', function() {
-  hamburgerBars.classList.toggle( 'toggle-navigation-view' );
-  headerNavigation.classList.toggle( 'toggle-navigation-view' );
+  hamburgerBars.classList.toggle( 'is-view' );
+  headerNavigation.classList.toggle( 'is-view' );
 });
 for( let index = 0; index < headerNavigationList.length; index++ ) {
   headerNavigationList[index].addEventListener( 'click', function() {
     if( headerNavigationList[index].querySelector( 'ul' ) != null ) {
-      headerNavigationList[index].querySelector( 'ul' ).classList.toggle( 'toggle-navigation-list' );
+      headerNavigationList[index].querySelector( 'ul' ).classList.toggle( 'is-view' );
     }
   });
 }
@@ -32,16 +32,16 @@ window.addEventListener( 'scroll', function() {
   let scrollValue = document.scrollingElement.scrollTop;
   if( header !== null ) {
     if( scrollValue > 0 ) {
-      header.classList.add( 'toggle-header-background' );
-      headerNavigation.classList.add( 'toggle-header-background' );
-      headerLoginUser.classList.add( 'toggle-header-background' );
-      headerToggleButton.classList.add( 'toggle-header-background' );
+      header.classList.add( 'is-notTop' );
+      headerNavigation.classList.add( 'is-notTop' );
+      headerLoginUser.classList.add( 'is-notTop' );
+      headerToggleButton.classList.add( 'is-notTop' );
     }
     else {
-      header.classList.remove( 'toggle-header-background' );
-      headerNavigation.classList.remove( 'toggle-header-background' );
-      headerLoginUser.classList.remove( 'toggle-header-background' );
-      headerToggleButton.classList.remove( 'toggle-header-background' );
+      header.classList.remove( 'is-notTop' );
+      headerNavigation.classList.remove( 'is-notTop' );
+      headerLoginUser.classList.remove( 'is-notTop' );
+      headerToggleButton.classList.remove( 'is-notTop' );
     }
   }
 });
