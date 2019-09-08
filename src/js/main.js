8b7282c31dcc0aff65b49e58647a0aa063b18340
Hiratake <hiratake@hiratake.xyz>
2019-09-08 15:19:48 +0900

    Update: モバイルでの1段目開閉を実装✨

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,11 @@
 
   'use strict';
 
+  var hamburger = document.getElementsByClassName( 'p-globalNavigation__hamburger' )[0];
+  var globalNavigation = document.getElementsByClassName( 'p-globalNavigation__container' )[0];
+  hamburger.addEventListener( 'click', function() {
+    hamburger.querySelector( 'span' ).classList.toggle( 'is-view' );
+    globalNavigation.classList.toggle( 'is-view' );
+  });
+
 })();
