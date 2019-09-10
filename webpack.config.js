7f633071fccfbfcf35efb278025ce6ef5e9266ee
Hiratake <hiratake@hiratake.xyz>
2019-09-10 23:04:38 +0900

    Add: babelの導入🎉

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,10 @@ module.exports = {
   output: {
     path    : __dirname + "/dist/js",
     filename: "main.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   }
 };
